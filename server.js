671a2dae7107f46737f174fd932bbe3d6e396f20
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-27 04:28:39 -0500

    added login hack + installed and used cookie-session

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
+const cookieSession = require('cookie-session');
+const app = express();
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -24,7 +25,11 @@ module.exports = {
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
-
+app.use(cookieSession({
+  name: 'session',
+  keys: ['Larry', 'Rocks'],
+  maxAge: 12 * 60 * 60 * 1000, // expires after 12 hrs
+}));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
@@ -56,6 +61,11 @@ app.use("/maps", mapsRoutes());
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get('/login/:id', (req, res) => {
+  req.session.userID = req.params.id;
+  res.redirect('/');
 });
 
 app.listen(PORT, () => {
