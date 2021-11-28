ae7df13b6a1d2b376cbe43ec4fcf44d1ddaeda7e
LeonXZhou <leon.zhou@queensu.ca>
2021-11-28 01:20:06 +0000

    implemented createMap and attachPinPlacementListenere.js

---
+++ public/scripts/attachPinPlacementListener.js
@@ -0,0 +1,18 @@
+const attachPinPlacementListener = function (map) {
+let marker = null;
+
+  const mapClick = map.addListener("click", (event) => {
+    if (!marker){
+      console.log(event.latLng.toJSON())
+      marker = new google.maps.Marker({
+        position: event.latLng.toJSON(),
+        map,
+      });
+    } else
+    {
+      marker.setPosition(event.latLng.toJSON());
+    }
+  })
+  return mapClick
+
+}
