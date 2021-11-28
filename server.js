bf2e0b9cafca3f05d327c27c535940568849a802
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-28 08:01:21 -0500

    small styling edit to fit more maps in homepage for function implementation

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
