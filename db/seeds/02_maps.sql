efa863dbaa475ccd293d7b285807318d13e8d001
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-28 07:58:19 -0500

    edited descriptions for map seeds to distinguish better

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,11 +1,11 @@
 INSERT INTO maps (user_id, title, description, is_public)
 VALUES
-(1, 'Adams Map1', 'SHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEESH', true),
-(1, 'Adams Map2', 'SHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEESH', true),
-(1, 'Adams Secret Map', 'SHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEESH', false),
-(2, 'Leons Map1', 'SHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEESH', true),
-(2, 'Leons Map2', 'SHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEESH', true),
-(2, 'Leons Secret Map', 'SHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEESH', false),
-(3, 'Josephs Map1', 'SHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEESH', true),
-(3, 'Josephs Map2', 'SHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEESH', true),
-(3, 'Josephs Secret Map', 'SHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEESH', false);
+(1, 'Adams Map1', 'Awesome map description1', true),
+(1, 'Adams Map2', 'map 2 description', true),
+(1, 'Adams Secret Map', 'Awesome secret map description', false),
+(2, 'Leons Map1', 'leons map description', true),
+(2, 'Leons Map2', 'second map desc for leon', true),
+(2, 'Leons Secret Map', 'secret mapppp', false),
+(3, 'Josephs Map1', 'sheeeeeeeeeesh', true),
+(3, 'Josephs Map2', 'desc 3 for josephs mapppppppppppppp', true),
+(3, 'Josephs Secret Map', 'invisible description', false);
