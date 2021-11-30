30c3488a4fb05ef07f7b6059899129bd74564be0
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-30 08:50:32 -0500

    add edit map page

--- public/scripts/components/edit-map.js
+++ public/scripts/components/edit-map.js
@@ -1,3 +1,12 @@
+/* eslint-disable no-undef */
+
 $(() => {
-$()
-})
+  $(`#submit`).on('submit', function(event) {
+    event.preventDefault();
+    const data = $(this).serialize();
+    const mapId = window.googleMarker.map.mapId;
+    console.log(data);
+    addPin(mapId, data)
+      .then($('#edit-map').trigger('reset'));
+  });
+});
