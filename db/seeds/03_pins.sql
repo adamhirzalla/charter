7265a4530f4617b07102b8c84537ccd167a005f6
Joseph Micla <josephmicla@gmail.com>
2021-11-30 14:10:01 -0800

    adds titles to pins seed

--- db/seeds/03_pins.sql
+++ db/seeds/03_pins.sql
@@ -1,11 +1,11 @@
-INSERT INTO pins (map_id, user_id, lat, long, icon, description)
+INSERT INTO pins (map_id, user_id, title, lat, long, icon, description)
 VALUES
-(1, 1, 45.42306195297514, -75.68680077244888, '/images/icons/poke-marker.svg', 'This is a cool pin'),
-(1, 1, 45.41821228984041, -75.68311005285348, '/images/icons/pokeball.svg', 'Great pin desc'),
-(3, 1, 45.417941178365155, -75.70091992066848, '/images/icons/duck.svg', 'Amazing pin desc'),
-(4, 2, 45.41667597413467, -75.70602684666197, '/images/icons/poke-marker.svg', 'awesome pin desc'),
-(5, 2, 45.41028927316074, -75.69731503180306, '/images/icons/pokeball.svg', 'Woah Pin Description'),
-(6, 2, 45.406824486977385, -75.70628433872677, '/images/icons/duck.svg', 'Yay Pin Description'),
-(8, 3, 45.40896364080057, -75.68654328038339, '/images/icons/poke-marker.svg', 'UwU Pin Description'),
-(8, 3, 45.4132417057994, -75.68057804754328, '/images/icons/pokeball.svg', 'NOWAY Pin Description'),
-(9, 3, 45.41890512429068, -75.69611340214135, '/images/icons/duck.svg', 'Boring Pin Description');
+(1, 1, 'A Great Spot', 45.42306195297514, -75.68680077244888, '/images/icons/poke-marker.svg', 'This is a cool pin'),
+(1, 1, 'I Hate This Spot', 45.41821228984041, -75.68311005285348, '/images/icons/pokeball.svg', 'Great pin desc'),
+(3, 1, 'An Interesting Find', 45.417941178365155, -75.70091992066848, '/images/icons/duck.svg', 'Amazing pin desc'),
+(4, 2, 'Unique Landscape', 45.41667597413467, -75.70602684666197, '/images/icons/poke-marker.svg', 'awesome pin desc'),
+(5, 2, 'Bigfoot', 45.41028927316074, -75.69731503180306, '/images/icons/pokeball.svg', 'Woah Pin Description'),
+(6, 2, 'A Wild Eevee Appeared!', 45.406824486977385, -75.70628433872677, '/images/icons/duck.svg', 'Yay Pin Description'),
+(8, 3, 'A Bird? A Plane?', 45.40896364080057, -75.68654328038339, '/images/icons/poke-marker.svg', 'UwU Pin Description'),
+(8, 3, 'Lavender Town', 45.4132417057994, -75.68057804754328, '/images/icons/pokeball.svg', 'NOWAY Pin Description'),
+(9, 3, 'Lighthouse Labs', 45.41890512429068, -75.69611340214135, '/images/icons/duck.svg', 'Boring Pin Description');
