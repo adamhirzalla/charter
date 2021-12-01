26a76445bae9f1b0fc9fce1eb408a240cc6f9d32
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-01 00:55:54 -0500

    add theme support for maps + only render a map if container for it exists

--- public/scripts/createMap.js
+++ public/scripts/createMap.js
@@ -1,18 +1,31 @@
 /* eslint-disable no-undef */
 
+
 const createMap = function(map) {
-  let googleMap = new google.maps.Map(document.querySelector(`#map-${map.id}`), {
-    zoom: 12,
-    center: { lat: 45.42182698501992, lng: -75.69454699197138 },
-    mapId: '93b06d228f001f87',
-    mapTypeControl: false,
-    fullscreenControl: false,
-    streetViewControl: false,
-    mapID: map.id
-  });
-  getAllMapPins(map.id)
-    .then(pins => {
-      drawPins(pins, googleMap);
+  const theme = getTheme(map.theme);
+  if ($(`#map-${map.id}`).length) {
+    let googleMap = new google.maps.Map(document.querySelector(`#map-${map.id}`), {
+      zoom: 12,
+      center: { lat: 45.42182698501992, lng: -75.69454699197138 },
+      mapId: theme,
+      mapTypeControl: false,
+      fullscreenControl: false,
+      streetViewControl: false,
+      mapID: map.id
     });
-  return googleMap;
+    getAllMapPins(map.id)
+      .then(pins => {
+        drawPins(pins, googleMap);
+      });
+    return googleMap;
+  }
+};
+
+const getTheme = (theme) => {
+  switch (theme) {
+  case 'pokemon':
+    return '93b06d228f001f87';
+  case 'default':
+    return null;
+  }
 };
