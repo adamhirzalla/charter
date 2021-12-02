8f5838284517194880ed5adc6e1cf151f74fa72c
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-02 00:31:23 -0500

    add pin update/edit

--- public/scripts/drawPins.js
+++ public/scripts/drawPins.js
@@ -6,26 +6,49 @@ const drawPins = (pins, map) => {
     const marker = new google.maps.Marker({
       position: { lat: pin.lat, lng: pin.long },
       map,
-      title: `Pin ${pin.id}!`,
+      title: pin.title,
       animation: google.maps.Animation.DROP,
       icon: {
         url: pin.icon,
         scaledSize: new google.maps.Size(40, 55)
       },
-      pinId: pin.id
+      pin
     });
-    addInfoWindow(map, pin.description, marker);
+    addInfoWindow(marker, map);
   }
 };
 
-const addInfoWindow = (map, content, marker) => {
+const addInfoWindow = (marker, map) => {
+  const {pin} = marker;
+  const content = `
+  <h1>${pin.title}</h1>
+  <p>${pin.description}</p>
+  `;
   const infoWindow = new google.maps.InfoWindow({content});
   marker.addListener("click", () => {
     window.selectedMarker = marker;
+    marker.setDraggable(true);
     infoWindow.open({
       map,
       anchor: marker,
       shouldFocus: false,
     });
+    const icon = getIcon(pin.icon);
+    $('#title').val(pin.title);
+    $('#description').val(pin.description);
+    $('#image-url').val(pin.image);
+    $('#lat').val(Math.round(marker.getPosition().toJSON().lat * 10000) / 10000);
+    $('#long').val(Math.round(marker.getPosition().toJSON().lng * 10000) / 10000);
+    $(`#${icon}`).prop('checked', true);
   });
+};
+const getIcon = (path) => {
+  switch (path) {
+  case '/images/icons/larry.gif':
+    return 'larry';
+  case '/images/icons/pokeball.svg':
+    return 'pokeball';
+  case '/images/icons/poke-marker.svg':
+    return 'default';
+  }
 };
